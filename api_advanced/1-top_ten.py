a4b7862f3d54a894ba13fe908d0210d9cb127cae
jmuhire13 <j.muhire@alustudent.com>
2025-02-18 07:56:35 +0000

    wapiri

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,38 +1,18 @@
 #!/usr/bin/python3
-"""
-1-top_ten.py
-"""
-
+"""API"""
 import requests
 
+
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the first 10 hot posts for a given subreddit.
+    """API"""
+    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
+        .format(subreddit)
+    headers = headers = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(reddit_url, headers=headers)
 
-    Args:
-        subreddit (str): The name of the subreddit.
-
-    Returns:
-        None
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {"User-Agent": "Mozilla/5.0"}  # Custom User-Agent to avoid rate limiting
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            posts = data["data"]["children"]
-            for i, post in enumerate(posts[:10], 1):
-                print(post["data"]["title"])
-        else:
-            print(None)
-    except requests.RequestException:
-        print(None)
-
-if __name__ == '__main__':
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
+    if response.status_code == 200:
+        data = response.json()['data']
+        for post in data['children'][:10]:
+            print(post['data']['title'])
     else:
-        top_ten(sys.argv[1])
+        print(None)
